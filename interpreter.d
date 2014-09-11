3213e5eccfa597e0c87fdb30aca71ce0bc185978
Cédric Picard <mystiro@gmail.com>
2014-09-11 11:18:04 +0200

    Splitted the interpreter as well

--- interpreter.d
+++ interpreter.d
@@ -1,10 +1,5 @@
 #!/usr/bin/env rdmd
 
-import std.stdio;
-import std.ascii;
-import std.c.process;
-import std.conv;
-
 /*  Syntax and specifications:
     --------------------------
 
@@ -21,8 +16,32 @@ import std.conv;
     The program must end with a dot '.'
 */
 
-char LOOK;
+import std.stdio;
+import std.ascii;
+import std.c.process;
+import std.conv;
+import utils;
+
 int[string] TABLE;
+
+int getNum()
+{
+    string value = "";
+    string retval;
+
+    if (!isDigit(LOOK))
+        expected("Integer");
+
+    while (isDigit(LOOK)) {
+        value ~= LOOK;
+        getChar();
+    }
+
+    retval = value;
+    skipWhite();
+    return to!int(value);
+}
+
 
 void initTable()
 {
@@ -31,103 +50,6 @@ void initTable()
     }
 }
 
-void getChar()
-{
-    readf("%c", &LOOK);
-}
-
-void error(string msg)
-{
-    stderr.write("Error: " ~ msg ~ '\n');
-}
-
-void abort(string msg)
-{
-    error(msg);
-    exit(1);
-}
-
-void expected(string msg)
-{
-    abort(msg ~ " expected");
-}
-
-void match(char c)
-{
-    if (LOOK != c)
-        expected("'" ~ c ~ "'");
-
-    else {
-        getChar();
-        skipWhite();
-    }
-}
-
-bool isAddop(char c)
-{
-    return LOOK == '+' || LOOK == '-';
-}
-
-bool isMulop(char c)
-{
-    return LOOK == '*' || LOOK == '/';
-}
-
-bool isAlNum(char c)
-{
-    return isAlpha(c) || isDigit(c);
-}
-
-string getName()
-{
-    string token = "";
-    string retval;
-
-    if (!isAlpha(LOOK))
-        expected("Name");
-
-    while (isAlNum(LOOK)) {
-        token ~= LOOK.toUpper();
-        getChar();
-    }
-
-    retval = token;
-    skipWhite();
-    return token;
-}
-
-int getNum()
-{
-    int value = 0;
-    if (!isDigit(LOOK))
-        expected("Integer");
-
-    while (isDigit(LOOK)) {
-        value = 10 * value + LOOK - '0';
-        getChar();
-    }
-    return value;
-}
-
-void skipWhite()
-{
-    while (LOOK == ' ' || LOOK == '\t') {
-        getChar();
-    }
-}
-
-void emit(string msg)
-{
-    write("\t" ~ msg);
-}
-
-void emitln(string msg)
-{
-    emit(msg);
-    writeln();
-}
-
-/*******************************************************/
 int term()
 {
     int value = factor();
